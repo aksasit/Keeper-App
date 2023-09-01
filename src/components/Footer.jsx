c4bc7c8b2fe6ce36481608a43079f652d8320c19
asit <sahooasit621@gmail.com>
2023-09-01 16:22:27 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {  
+  const currentYear = new Date().getFullYear()  
+  return (
+    <div>
+      <footer>
+        <p>Copyright ⓒ {currentYear}</p>
+      </footer>      
+    </div>
+  )
+}
+
+export default Footer
